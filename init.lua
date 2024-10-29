f5312178c22e121d51fd2ce443dda7f8a23443d6
Ajitesh Panda <aji.yash13@gmail.com>
2024-10-29 15:24:14 +0530

    keybinding added for moving line up and down

--- init.lua
+++ init.lua
@@ -37,3 +37,8 @@ require "nvchad.autocmds"
 vim.schedule(function()
   require "mappings"
 end)
+
+vim.keymap.set('n', '<A-j>', ':m .+1<CR>==')
+vim.keymap.set('n', '<A-k>', ':m .-2<CR>==')
+vim.keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv")
